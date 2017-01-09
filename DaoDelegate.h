a0b40246e1fb11d142f12be14de92fede4964bc5
Juliana Strawn <JulianaStrawn@Juliana-Strawns-MacBook-Pro.local>
2017-01-09 17:23:05 -0500

    trello card 5

---
+++ DaoDelegate.h
@@ -0,0 +1,13 @@
+//
+//  DaoDelegate.h
+//  NavCtrl
+//
+//  Created by Juliana Strawn on 1/9/17.
+//  Copyright © 2017 Aditya Narayan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DaoDelegate <NSObject>
+
+@end
